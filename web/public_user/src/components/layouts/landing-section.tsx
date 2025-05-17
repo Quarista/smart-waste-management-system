1d13c8a2047098b1a0e508a02526ac4704b283fa
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-05-18 00:13:45 +0530

    Create landing-section.tsx

---
+++ web/public_user/src/components/layouts/landing-section.tsx
@@ -0,0 +1,31 @@
+{/*}
+import { ContainerScroll } from "@/components/animations/section-scroll-animation";
+
+export function LandingScroll() {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Scroll Animations
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <img
+          src="https://framerusercontent.com/images/kE6W96xkAKEXswgHZFFnQZOt910.jpg"
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
+*/}
