b813e08285e93ae7c5ec030a124f6b014d3e80f0
Randeepa Jayasekara <147414352+randeepajayasekara@users.noreply.github.com>
2025-05-18 00:06:29 +0530

    Create router_names.dart

---
+++ mobile/flutter/lib/router/router_names.dart
@@ -0,0 +1,12 @@
+class RouterNames {
+  final String main = '/';
+  final String splash = '/splash';
+  final String home = '/home';
+  final String bin = '/bin';
+  final String bind = '/bind';
+  final String createpost = '/createpost';
+  final String createbin = '/createbin';
+  final String quarista = '/quarista';
+  final String trip = '/trip';
+  final String sessionexpired = '/sessionexpired';
+}
